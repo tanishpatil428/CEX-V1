25fe3170e7c61ab7e8416055e90e4dcc80454394
Tanish <tanishpatil428@gmail.com>
2026-08-23 11:04:39 +0530

    added controller for testing

--- backend/src/controller/balances.controller.ts
+++ backend/src/controller/balances.controller.ts
@@ -1,5 +1,6 @@
 import type{ Request,Response } from "express";
-import { deposit ,getBalance , lockBalance ,unlockBalance} from "../engine/balances";
+import { deposit ,getBalance , lockBalance ,unlockBalance,depositStock} from "../engine/balances";
+
 
 export const depositController = (req:Request ,res:Response) =>{
     const userid = req.userId
@@ -8,7 +9,7 @@ export const depositController = (req:Request ,res:Response) =>{
    const deposited = deposit(userid ,amount)
 
     return res.status(200).json(deposited)
-}
+} 
 
  export const getBalancesController = (req:Request, res:Response) =>{
     const userid = req.userId
@@ -17,6 +18,10 @@ export const depositController = (req:Request ,res:Response) =>{
 
    return res.status(200).json(balance)
 }
+
+
+
+//this routes are for testing purpose not for real use
 
 export const lockBalanceController = (req:Request, res:Response) =>{
 
@@ -35,4 +40,15 @@ export const unlockBalanceController = (req:Request, res:Response)=>{
     const unlock = unlockBalance(userId ,amount)
 
     return res.status(200).json(unlock)
+}
+
+
+export const depositStockController = (req:Request, res:Response)=>{
+    const userId = req.userId
+   const {symbol,qty}=  req.body
+    
+    const addstock = depositStock(symbol,Number(qty),userId)
+
+    return res.json(addstock)
+
 }

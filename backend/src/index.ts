ad5ad313ce9218ccbcd441fbeb334aaf04c5a5fc
Tanish <tanishpatil428@gmail.com>
2026-08-23 11:02:32 +0530

    json error handel & order route connected

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import route from './routes/auth.route'
 import balances  from './routes/balances.route'
 import { authmiddleware } from './middleware/auth.middleware'
 import order from "./routes/order.route"
+import type{ Request,Response,NextFunction } from 'express'
 
 import "dotenv/config"
 
@@ -10,9 +11,19 @@ import "dotenv/config"
 const app = express()
 
 app.use(express.json())
+app.use((err:any, req:Request, res:Response, next:NextFunction) => {
+    if (err.type === 'entity.parse.failed') {
+      return res.status(400).json({ error: 'Invalid JSON in request body' });
+    }
+    next(err);
+  });
+
+
+
 app.use('/auth',route)
 app.use('/balances' ,authmiddleware, balances)
 app.use('/order' ,authmiddleware ,order)
+
 
 
 const PORT = process.env.PORT

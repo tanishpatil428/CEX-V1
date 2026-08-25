047c37ac4de1b169f79e7c27e6f906f30e503212
Tanish <tanishpatil428@gmail.com>
2026-08-25 10:42:59 +0530

    var name change

--- backend/src/valditation/orders.validation.ts
+++ backend/src/valditation/orders.validation.ts
@@ -1,6 +1,6 @@
 import {z} from 'zod'
 
-export const orderValidation =z.object({
+export const placeOrderSchema =z.object({
     symbol :z.string().min(1 ,"symbol is requried"),
     price :z.number().positive("price must be positive"),
     qty : z.number().positive("Quantity must be positive"),

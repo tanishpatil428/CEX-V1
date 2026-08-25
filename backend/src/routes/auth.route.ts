7070336b852e290982a82b1d4d6c1748a81c5bbd
Tanish <tanishpatil428@gmail.com>
2026-08-25 10:42:12 +0530

    file name change apply

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { signup,signin } from "../controller/auth.controller";
-import { valditation } from "../middleware/auth.validate.middleware";
+import { valditation } from "../middleware/validate.middleware";
 import { signupSchema, signinSchema } from "../valditation/auth.validation";
 
  const route = Router()
